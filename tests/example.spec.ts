45f82024b7cb812ca6ed3c5bfd2af8dfa4b1c87d
Adilla <adilla@gmail.com>
2025-01-22 11:28:43 +0700

    test : modify code on example.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,13 +7,16 @@ import { PlaywrightSidebarPage } from "./dashboard/sidebar";
 import { PlaywrightPembeliPage } from "./dashboard/pembeli/pembeli";
 import { PlaywrightPembeliEditPage } from "./dashboard/pembeli/editpembeli";
 import { PlaywrightLihatPembeliPage } from "./dashboard/pembeli/lihatpembeli";
+import { PlaywrightHapusPembeliPage } from "./dashboard/pembeli/hapuspembeli";
 
 import { PlaywrightBeliCashPage } from "./dashboard/belicash/belicash";
 import { PlaywrightBeliCashEditPage } from "./dashboard/belicash/editbelicash";
+import { PlaywrightHapusBeliCashPage } from "./dashboard/belicash/hapusbelicash";
 
 import { PlaywrightBeliKreditPage } from "./dashboard/belikredit/belikredit";
 import { PlaywrightBeliKreditEditPage } from "./dashboard/belikredit/editbelikredit";
 import { PlaywrightLihatBeliKreditPage } from "./dashboard/belikredit/lihatbelikredit";
+import { PlaywrightHapusBeliKreditPage } from "./dashboard/belikredit/hapusbelikredit";
 
 const emailUser = "adilla@gmail.com";
 const passUser = "adilla0306";
@@ -26,11 +29,14 @@ test.describe("motomarker", () => {
   let pembeliPage: PlaywrightPembeliPage;
   let editpembeliPage: PlaywrightPembeliEditPage;
   let lihatpembeliPage: PlaywrightLihatPembeliPage;
+  let hapuspembeliPage: PlaywrightHapusPembeliPage;
   let belicashPage: PlaywrightBeliCashPage;
   let editbelicashPage: PlaywrightBeliCashEditPage;
+  let hapusBeliCashPage: PlaywrightHapusBeliCashPage;
   let belikreditPage: PlaywrightBeliKreditPage;
   let editbelikreditPage: PlaywrightBeliKreditEditPage;
   let lihatbelikreditPage: PlaywrightLihatBeliKreditPage;
+  let hapusBeliKreditPage: PlaywrightHapusBeliKreditPage;
 
   test.beforeEach(async ({ page }) => {
     dashboardPage = new PlaywrightDashboardPage(page);
@@ -40,11 +46,14 @@ test.describe("motomarker", () => {
     pembeliPage = new PlaywrightPembeliPage(page);
     editpembeliPage = new PlaywrightPembeliEditPage(page);
     lihatpembeliPage = new PlaywrightLihatPembeliPage(page);
+    hapuspembeliPage = new PlaywrightHapusPembeliPage(page);
     belicashPage = new PlaywrightBeliCashPage(page);
     editbelicashPage = new PlaywrightBeliCashEditPage(page);
+    hapusBeliCashPage = new PlaywrightHapusBeliCashPage(page);
     belikreditPage = new PlaywrightBeliKreditPage(page);
     editbelikreditPage = new PlaywrightBeliKreditEditPage(page);
     lihatbelikreditPage = new PlaywrightLihatBeliKreditPage(page);
+    hapusBeliKreditPage = new PlaywrightHapusBeliKreditPage(page);
 
     await page.goto("http://127.0.0.1:8000/login");
 
@@ -60,23 +69,15 @@ test.describe("motomarker", () => {
 
     await sidebarPage.cekPembeli();
     await dashboardPage.cekKontenPembeli();
-    await dashboardPage.cekTambahPembeli();
-    await sidebarPage.cekEditPembeli();
-    await sidebarPage.cekLihatPembeli();
 
     await sidebarPage.cekBeliCash();
     await dashboardPage.cekKontenBeliCash();
-    await dashboardPage.cekTambahTransaksiCash();
-    await sidebarPage.cekEditBeliCash();
 
     await sidebarPage.cekKreditPaket();
     await dashboardPage.cekKontenKreditPaket();
 
     await sidebarPage.cekBeliKredit();
     await dashboardPage.cekKontenBeliKredit();
-    await dashboardPage.cekTambahTransaksiKredit();
-    await sidebarPage.cekEditBeliKredit();
-    await sidebarPage.cekLihatBeliKredit();
 
     await sidebarPage.cekBayarCicilan();
     await dashboardPage.cekKontenBayarCicilan();
@@ -86,25 +87,17 @@ test.describe("motomarker", () => {
 
   test("submit pembeli form", async ({ page }) => {
     await sidebarPage.cekPembeli();
-    await dashboardPage.cekTambahPembeli();
-    await pembeliPage.inputpembeli(
-      "1234567890123456",
-      "Adilla Ibrahim",
-      "Cimahi, Indonesia",
-      "08123456789"
-    );
+    await pembeliPage.inputpembeli();
     await pembeliPage.submitFormPembeli();
     await pembeliPage.MemastikanPembeliMasuk();
   });
 
   test("submit transaksi cash form", async ({ page }) => {
     await sidebarPage.cekBeliCash();
-    await dashboardPage.cekTambahTransaksiCash();
     await belicashPage.inputbelicash(
-      "TXC-001",
       "NMAX",
       "25000000",
-      "Adilla",
+      "Adilla Ibrahim",
       "2025-01-20"
     );
     await belicashPage.submitFormBeliCash();
@@ -113,15 +106,13 @@ test.describe("motomarker", () => {
 
   test("submit transaksi kredit form", async ({ page }) => {
     await sidebarPage.cekBeliKredit();
-    await dashboardPage.cekTambahTransaksiKredit();
     await belikreditPage.inputbelikredit(
-      "K002",
-      "Renataa",
+      "Adilla Ibrahim",
       "Beat",
-      "P002",
       "2025-01-01",
+      "P002",
       "Ya",
-      "Ya",
+      "Tidak",
       "Tidak"
     );
     await belikreditPage.submitFormBeliKredit();
@@ -129,26 +120,37 @@ test.describe("motomarker", () => {
   });
 
   test("edit pembeli form", async ({ page }) => {
-    await sidebarPage.cekEditPembeli();
-    await dashboardPage.cekEditPembeli();
-    await editpembeliPage.editPembeli("Adilla", "Baros");
+    await editpembeliPage.editPembeli("Adilla Ibrahim");
     await editpembeliPage.submitFormEditPembeli();
     await editpembeliPage.MemastikanEditPembeliMasuk();
+    await lihatpembeliPage.goToLihatPembeli();
   });
 
   test("edit beli cash form", async ({ page }) => {
-    await sidebarPage.cekEditBeliCash();
-    await dashboardPage.cekEditBeliCash();
-    await editbelicashPage.editbelicash("Beat", "Renataa");
+    await editbelicashPage.editbelicash("Beat", "Adilla Ibrahim");
     await editbelicashPage.submitFormEditBeliCash();
     await editbelicashPage.MemastikanEditBeliCashMasuk();
   });
 
   test("edit beli kredit form", async ({ page }) => {
-    await sidebarPage.cekEditBeliKredit();
-    await dashboardPage.cekEditBeliKredit();
     await editbelikreditPage.editbelikredit("P002", "Tidak");
     await editbelikreditPage.submitFormEditBeliKredit();
     await editbelikreditPage.MemastikanEditBeliKreditMasuk();
+    await lihatbelikreditPage.goToLihatBeliKredit();
+  });
+
+  test("hapus pembeli", async () => {
+    await sidebarPage.cekPembeli();
+    await hapuspembeliPage.hapusPembeli();
+  });
+
+  test("hapus beli cash", async () => {
+    await sidebarPage.cekBeliCash();
+    await hapusBeliCashPage.hapusBeliCash();
+  });
+
+  test("hapus beli kredit", async () => {
+    await sidebarPage.cekBeliKredit();
+    await hapusBeliKreditPage.hapusBeliKredit();
   });
 });
