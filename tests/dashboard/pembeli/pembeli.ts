051b597b80dbfa310f0df93cb7cb66d5131d916c
Adilla <adilla@gmail.com>
2025-02-07 09:45:55 +0700

    test: remove unused Pembeli edit, delete, and view classes #1 - integrate functionality into PlaywrightPembeliPage

--- tests/dashboard/pembeli/pembeli.ts
+++ tests/dashboard/pembeli/pembeli.ts
@@ -4,26 +4,55 @@ import { faker } from "@faker-js/faker/locale/id_ID";
 export class PlaywrightPembeliPage {
   readonly page: Page;
   readonly daftarPembeli: Locator;
-  readonly editButton: Locator;
   readonly tambahPembeli: Locator;
   readonly noKtpInput: Locator;
   readonly namaInput: Locator;
   readonly alamatInput: Locator;
   readonly telponInput: Locator;
+
+  readonly editPembeliButton: Locator;
+  readonly editnamaInput: Locator;
+  readonly editalamatInput: Locator;
+
+  readonly lihatButton: Locator;
+  readonly kembaliButton: Locator;
   readonly simpanButton: Locator;
+
+  readonly HapusPembeliButton: Locator;
+  readonly KonfirmasiHapusButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.daftarPembeli = page.locator("h1", { hasText: "Daftar Pembeli" });
-    this.editButton = page.locator("button", { hasText: "Edit" });
     this.tambahPembeli = page.locator("a", { hasText: "Tambah Pembeli" });
     this.noKtpInput = page.locator("input#pembeli_No_KTP");
     this.namaInput = page.locator("input#pembeli_nama");
     this.alamatInput = page.locator("input#pembeli_alamat");
     this.telponInput = page.locator("input#pembeli_telpon");
+
+    this.editPembeliButton = page
+      .getByRole("row")
+      .locator("#editpembeli")
+      .first();
+    this.editnamaInput = page.locator("input#pembeli_nama");
+    this.editalamatInput = page.locator("input#pembeli_alamat");
+
+    this.lihatButton = page.locator("a", { hasText: "Lihat" });
+    this.kembaliButton = page.locator("button", { hasText: "Kembali" });
+
     this.simpanButton = page.locator("button", { hasText: "Simpan" });
+
+    this.HapusPembeliButton = page
+      .locator("button", {
+        hasText: "Hapus",
+      })
+      .first();
+    this.KonfirmasiHapusButton = page.getByRole("button", {
+      name: "Ya, Hapus!",
+    });
   }
 
+  //INPUT PEMBELI
   async inputpembeli(pembeli_nama: string) {
     const pembeli_No_KTP = faker.string.numeric(16);
     const pembeli_alamat = faker.location.streetAddress();
@@ -54,5 +83,47 @@ export class PlaywrightPembeliPage {
     await expect(
       this.page.locator("h1", { hasText: "Daftar Pembeli" })
     ).toBeVisible();
+  }
+
+  //EDIT PEMBELI
+  async editPembeli(pembeli_nama: string) {
+    const pembeli_alamat = faker.location.streetAddress();
+
+    await this.daftarPembeli.click();
+    await this.editPembeliButton.click();
+
+    await this.page.locator("input#pembeli_nama").waitFor({ state: "visible" });
+    await this.namaInput.fill(pembeli_nama);
+    await this.page
+      .locator("input#pembeli_alamat")
+      .waitFor({ state: "visible" });
+    await this.alamatInput.fill(pembeli_alamat);
+  }
+
+  async submitFormEditPembeli() {
+    await this.simpanButton.click();
+  }
+
+  async MemastikanEditPembeliMasuk() {
+    await expect(
+      this.page.locator("h1", { hasText: "Daftar Pembeli" })
+    ).toBeVisible();
+  }
+
+  //LIHAT PEMBELI
+  async goToLihatPembeli() {
+    await this.lihatButton.first().click();
+    await expect(
+      this.page.locator("h1", { hasText: "Lihat Pembeli" })
+    ).toBeVisible();
+    await this.kembaliButton.click();
+    await expect(this.daftarPembeli).toBeVisible();
+  }
+
+  //HAPUS PEMBELI
+  async hapusPembeli() {
+    await this.HapusPembeliButton.click();
+    await this.KonfirmasiHapusButton.click();
+    await expect(this.daftarPembeli).toBeVisible();
   }
 }
