74bb40771297fe02b657e29bb0b62dca569900ce
Adilla <adilla@gmail.com>
2025-02-11 08:04:00 +0700

    test: refactor PlaywrightBeliKreditPage to use FormBeliKredit interface for input and edit methods #2

--- tests/dashboard/belikredit/belikredit.ts
+++ tests/dashboard/belikredit/belikredit.ts
@@ -1,5 +1,6 @@
 import { expect, type Locator, type Page } from "@playwright/test";
 import { faker } from "@faker-js/faker/locale/id_ID";
+import { FormBeliKredit } from "../type/type";
 
 export class PlaywrightBeliKreditPage {
   readonly page: Page;
@@ -17,8 +18,9 @@ export class PlaywrightBeliKreditPage {
   readonly fotoGajiDropdown: Locator;
 
   readonly editBeliKreditPaket: Locator;
+  readonly editpembeliDropdown: Locator;
+  readonly editmotorDropdown: Locator;
   readonly editpaketKreditDropdown: Locator;
-  readonly editfotoKTPDropdown: Locator;
 
   readonly lihatButton: Locator;
   readonly kembaliButton: Locator;
@@ -38,20 +40,21 @@ export class PlaywrightBeliKreditPage {
       hasText: "Tambah Transaksi Kredit",
     });
     this.kodeTransaksiInput = page.locator("input#kridit_kode");
-    this.pembeliDropdown = page.locator("select#pembeli_No_KTP");
-    this.motorDropdown = page.locator("select#motor_kode");
-    this.paketKreditDropdown = page.locator("select#paket_kode");
-    this.tanggalInput = page.locator("input#kridit_tanggal");
+    this.pembeliDropdown = page.locator("select#pembeli_No_KTP").first();
+    this.motorDropdown = page.locator("select#motor_kode").first();
+    this.paketKreditDropdown = page.locator("select#paket_kode").first();
+    this.tanggalInput = page.locator("input#kridit_tanggal").first();
     this.fotoKTPDropdown = page.locator("select#fotokopi_KTP");
-    this.fotoKKDropdown = page.locator("select#fotokopi_KK");
-    this.fotoGajiDropdown = page.locator("select#fotokopi_slip_gaji");
+    this.fotoKKDropdown = page.locator("select#fotokopi_KK").first();
+    this.fotoGajiDropdown = page.locator("select#fotokopi_slip_gaji").first();
 
     this.editBeliKreditPaket = page
       .getByRole("row")
       .locator("#editbelikredit")
       .first();
-    this.editpaketKreditDropdown = page.locator("select#paket_kode");
-    this.editfotoKTPDropdown = page.locator("select#fotokopi_KTP");
+    this.editpembeliDropdown = page.locator("select#pembeli_No_KTP").last();
+    this.editmotorDropdown = page.locator("select#motor_kode").last();
+    this.editpaketKreditDropdown = page.locator("select#paket_kode").last();
     this.simpanButton = page.locator("button", { hasText: "Simpan" });
 
     this.daftarTransaksiKredit = page.locator("h1", {
@@ -72,25 +75,22 @@ export class PlaywrightBeliKreditPage {
 
   //INPUT BELI KREDIT
   async inputbelikredit(
+    InputBeliKredit: FormBeliKredit,
     pembeli_No_KTP: string,
     motor_kode: string,
-    kridit_tanggal: string,
-    paket_kode: string,
-    fotokopi_KTP: string,
-    fotokopi_KK: string,
-    fotokopi_slip_gaji: string
+    paket_kode: string
+    // fotokopi_KTP: string,
+    // fotokopi_KK: string,
+    // fotokopi_slip_gaji: string
   ) {
-    const kridit_kode = faker.string.numeric();
-
     await this.tambahTransaksiKredit.first().click();
     await this.page.locator("input#kridit_kode").waitFor({ state: "visible" });
-    await this.kodeTransaksiInput.fill(kridit_kode);
+    await this.kodeTransaksiInput.fill(InputBeliKredit.kridit_kode);
 
     await this.page
       .locator("select#pembeli_No_KTP")
       .waitFor({ state: "visible" });
-    await this.pembeliDropdown.first().selectOption({ label: pembeli_No_KTP });
-
+    await this.pembeliDropdown.selectOption({ label: pembeli_No_KTP });
     await this.page.locator("select#motor_kode").waitFor({ state: "visible" });
     await this.motorDropdown.selectOption({ label: motor_kode });
 
@@ -100,20 +100,25 @@ export class PlaywrightBeliKreditPage {
     await this.page
       .locator("input#kridit_tanggal")
       .waitFor({ state: "visible" });
-    await this.tanggalInput.fill(kridit_tanggal);
+    await this.tanggalInput.fill(String(InputBeliKredit.kridit_tanggal));
 
     await this.page
       .locator("select#fotokopi_KTP")
       .waitFor({ state: "visible" });
-    await this.fotoKTPDropdown.selectOption({ label: fotokopi_KTP });
+    await this.fotoKTPDropdown.click();
+    await this.fotoKTPDropdown.selectOption(InputBeliKredit.fotokopi_KTP);
 
     await this.page.locator("select#fotokopi_KK").waitFor({ state: "visible" });
-    await this.fotoKKDropdown.selectOption({ label: fotokopi_KK });
+    await this.fotoKKDropdown.click();
+    await this.fotoKKDropdown.selectOption(InputBeliKredit.fotokopi_KK);
 
     await this.page
       .locator("select#fotokopi_slip_gaji")
       .waitFor({ state: "visible" });
-    await this.fotoGajiDropdown.selectOption({ label: fotokopi_slip_gaji });
+    await this.fotoGajiDropdown.click();
+    await this.fotoGajiDropdown.selectOption(
+      InputBeliKredit.fotokopi_slip_gaji
+    );
   }
 
   async submitFormBeliKredit() {
@@ -127,19 +132,51 @@ export class PlaywrightBeliKreditPage {
   }
 
   //EDIT BELI KREDIT
-  async editbelikredit(paket_kode: string, fotokopi_KTP: string) {
+  async editbelikredit(
+    EditBeliKredit: FormBeliKredit,
+    pembeli_No_KTP: string,
+    motor_kode: string,
+    paket_kode: string
+  ) {
     await this.goToBeliKredit.click();
     await expect(
       this.page.locator("h1", { hasText: "Daftar Transaksi Kredit" })
     ).toBeVisible();
     await this.editBeliKreditPaket.click();
 
+    // await this.page.locator("input#kridit_kode").waitFor({ state: "visible" });
+    // await this.page
+    //   .locator("input#kridit_kode")
+    //   .evaluate((el) => el.removeAttribute("readonly"));
+    // await this.kodeTransaksiInput.fill(EditBeliKredit.kridit_kode);
+
+    await this.page
+      .locator("select#pembeli_No_KTP")
+      .waitFor({ state: "visible" });
+    await this.editpembeliDropdown.selectOption({ label: pembeli_No_KTP });
+    await this.page.locator("select#motor_kode").waitFor({ state: "visible" });
+    await this.editmotorDropdown.selectOption({ label: motor_kode });
+
     await this.page.locator("select#paket_kode").waitFor({ state: "visible" });
-    await this.paketKreditDropdown.selectOption({ label: paket_kode });
+    await this.editpaketKreditDropdown.selectOption({ label: paket_kode });
+
+    await this.page
+      .locator("input#kridit_tanggal")
+      .waitFor({ state: "visible" });
+    await this.tanggalInput.fill(String(EditBeliKredit.kridit_tanggal));
+
     await this.page
       .locator("select#fotokopi_KTP")
       .waitFor({ state: "visible" });
-    await this.fotoKTPDropdown.selectOption({ label: fotokopi_KTP });
+    await this.fotoKTPDropdown.selectOption(EditBeliKredit.fotokopi_KTP);
+
+    await this.page.locator("select#fotokopi_KK").waitFor({ state: "visible" });
+    await this.fotoKKDropdown.selectOption(EditBeliKredit.fotokopi_KK);
+
+    await this.page
+      .locator("select#fotokopi_slip_gaji")
+      .waitFor({ state: "visible" });
+    await this.fotoGajiDropdown.selectOption(EditBeliKredit.fotokopi_slip_gaji);
   }
 
   async submitFormEditBeliKredit() {
